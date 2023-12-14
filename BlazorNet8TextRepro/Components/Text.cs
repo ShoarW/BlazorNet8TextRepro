add527ac9a196fd45d1d2fd599537e9f368c3f15
Richard Pedersen <richardpd@olympic.co.nz>
2023-12-15 10:31:17 +1300

    Add project files.

---
+++ BlazorNet8TextRepro/Components/Text.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorNet8TextRepro.Components
+{
+    public class Text : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, "h1");
+            builder.AddContent(1, ChildContent);
+            builder.CloseElement();
+        }
+    }
+}
---
+++ BlazorNet8TextRepro/Components/Text2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorNet8TextRepro.Components;
+
+public class Text2 : ComponentBase
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, "h3");
+        builder.AddContent(1, ChildContent);
+        builder.CloseElement();
+    }
+}
